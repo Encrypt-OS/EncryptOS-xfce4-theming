19c6188ee1ff34f8e60e12d70a8e272ca105e0af
SnailDOS <46210117+snaildos@users.noreply.github.com>
2022-05-09 21:25:07 +0800

    Update links

--- xfce.sh
+++ xfce.sh
@@ -1,20 +1,19 @@
 #bin/bash
 
-echo "******* Installing EndeavourOS Theming for XFCE4*******" && sleep 5
+echo "******* Installing EncryptOS Theming for XFCE4*******" && sleep 5
 
-echo "******* cloning dotfiles for EndeavourOS - XFCE4 Theming *******" && sleep 1
-    git clone https://github.com/endeavouros-team/endeavouros-xfce4-theming.git
-    cd endeavouros-xfce4-theming
+echo "******* cloning dotfiles for EncryptOS - XFCE4 Theming *******" && sleep 1
+    git clone https://github.com/Encrypt-OS/encryptos-xfce4-theming.git
+    cd encryptos-xfce4-theming
 echo "******* Getting theme and packages installed now: *******" && sleep 1
-    wget -q --timeout=10 https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-packages-lists/master/xfce4
     sudo pacman -S --noconfirm --needed - < xfce4
 
 echo "******* setting up theme for Light-DM: *******" && sleep 1
-    wget -q --timeout=10 https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/airootfs/etc/lightdm/slick-greeter.conf
+    wget -q --timeout=10 https://raw.githubusercontent.com/Encrypt-OS/EncryptOS-ISO/main/airootfs/etc/lightdm/slick-greeter.conf
     sudo cp  slick-greeter.conf /etc/lightdm/
     
 echo "******* set lightdm.conf to logind-check-graphical=true ... enable slick greeter to be used *******" && sleep 1
-    cp /etc/lightdm/lightdm.conf ~/endeavouros-xfce4-theming/
+    cp /etc/lightdm/lightdm.conf ~/encryptos-xfce4-theming/
     sed -i 's?#logind-check-graphical=false?logind-check-graphical=true?' lightdm.conf
     sed -i 's?#greeter-session=example-gtk-gnome?greeter-session=lightdm-slick-greeter?' lightdm.conf
     sed -i 's?#allow-user-switching=true?allow-user-switching=true?' lightdm.conf
@@ -29,13 +28,13 @@ echo "******* setting up xfce4 theme and settings: *******" && sleep 1
     cp -R .config/ ~/
     dbus-launch dconf load / < xed.dconf
     cd ..
-    rm -rf endeavouros-xfce4-theming 
+    rm -rf encryptos-xfce4-theming 
 
 echo "******* All Done --- restarting System NOW! *******" && sleep 10
-echo "******* Please login again and enjoy EndeavourOS Theming! *******" && sleep 1
+echo "******* Please login again and enjoy EncryptOS Theming! *******" && sleep 1
 
     yad --title="Restarting System" \
-        --text="All done --- please login again and enjoy XFCE4 with EndeavourOS Theming!" \
+        --text="All done --- please login again and enjoy XFCE4 with EncryptOS Theming!" \
         --width=400 --height=100 \
         --button="Restart System":0
 
